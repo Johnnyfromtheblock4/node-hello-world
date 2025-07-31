0c3f76da14bcfde2c49a723a834931526282d34d
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-31 15:24:26 +0300

    [UPD] bonus

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Boolean");
+let param;
+if (process.argv[2] != undefined) {
+  param = process.argv[2];
+} else {
+  param = "World";
+}
+console.log(`Hello ${param}`);
+
+// FORMA CONTRATTA
+// let param = porocess.argv[2] != undefined ? process.argv[2] : "World";
+// console.log(`Hello ${param}`);
